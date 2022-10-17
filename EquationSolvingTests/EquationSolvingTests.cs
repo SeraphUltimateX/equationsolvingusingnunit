b26a76d91d2d05b223fce7733e1d5ae48dc65427
srx <SeraphUltimateX@gmail.com>
2022-10-17 09:48:35 +0700

    feature: quadratic equation using Po-Shen Loh; 4 forms of parameterized testing

--- EquationSolvingTests/EquationSolvingTests.cs
+++ EquationSolvingTests/EquationSolvingTests.cs
@@ -1,18 +1,52 @@
 using EquationSolving;
+using System.Net;
+using static EquationSolving.QuadraticEquation;
 
 namespace EquationSolvingTests
 {
-    public class LinearEquationTests
+    [TestFixture]
+    public class Tests
     {
-
-    }
-
-
-    public class QuadracticEquationTests
-    {
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+        }
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
         [SetUp]
         public void Setup()
         {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        private static readonly double[][] Coefficients =
+        {
+           new double[] {0, 8, 6565},
+           new double[] {0, 9.215454, -66662.1 },
+           new double[] {0, 0, 0 },
+           new double[] {0, -35451515, 69995965}
+        };
+
+        [Test]
+        public void TestSolvePoShenLohGivenWrongArgumentsThrowsException([ValueSource(nameof(Coefficients))] double[] coeffs)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => { Solve_Po_Shen_Loh(coeffs[0], coeffs[1], coeffs[2]); });
+        }
+
+        [Test]
+        [TestCase(4, 8, 3, new double[] { -1.5, -0.5 })]
+        [TestCase(1, 2, 1, new double[] { -1 })]
+        [TestCase(9, -0, 9, new double[] { })]
+        [TestCase(12, -50, 1, new double[] { 4.1465697338567, 0.020096932810009 })]
+        public void TestSolvePoShenLohGivenRightArgumentsReturnsWell(double x, double y, double z, double[] expected)
+        {
+            Assert.That(Solve_Po_Shen_Loh(4, 8, 3), Is.EquivalentTo(new double[] { -1.5, -0.5 }));
         }
 
         [Test, Sequential]
@@ -22,26 +56,17 @@ namespace EquationSolvingTests
             [Values(3, 10, 0)] double z,
             [Values(new double[] { -1.5, -0.5 },
                     new double[] {},
-                    new double[] { 0 })] double[] expected)
-        {
-            /*Assert.AreEqual(new double[] { -1.5, -0.5 }, QuadraticEquation.Solve(x, y, z));*/
-            Assert.That(expected, Is.EquivalentTo(QuadraticEquation.Solve(x, y, z)));
-        }
+                    new double[] { 0 })] double[] expected) =>
+            Assert.That(Solve(x, y, z), Is.EquivalentTo(expected));
 
         [Test]
         [TestCase(0, 5, 9)]
         [TestCase(0, 5, 0)]
         [TestCase(0, 0, 9)]
         [TestCase(0, 0, 0)]
-        public void TestSolveGivenWrongArgumentsThrowsException(double x, double y, double z)
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => { QuadraticEquation.Solve(x, y, z); });
-        }
+        public void TestSolveGivenWrongArgumentsThrowsException(double x, double y, double z) => Assert.Throws<ArgumentOutOfRangeException>(() => { Solve(x, y, z); });
 
-    }
 
-    public class CubicEquationTests
-    {
-
+        /*[TestCaseSource]*/
     }
 }
