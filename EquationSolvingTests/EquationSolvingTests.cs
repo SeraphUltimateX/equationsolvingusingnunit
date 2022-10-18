ab743e81a94afafba9d6a0552440dc64826aefc2
donework-now <huynhanhkiet2222@gmail.com>
2022-10-18 22:23:38 +0700

    update conflict

--- EquationSolvingTests/EquationSolvingTests.cs
+++ EquationSolvingTests/EquationSolvingTests.cs
@@ -92,31 +92,29 @@ namespace EquationSolvingTests
                 Assert.Throws<ArgumentOutOfRangeException>(() => { LinearEquation.LinearEquationSolve(a, b); });
             }
         }
-            public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
-        [Test, Sequential]//Test case 1
-        public void TestSolvePTB3GivenRightArgumentsReturnsWell(
-            [Values(1, 0, 1)] double x,
-            [Values(1, 2, 0)] double y,
-            [Values(-3, 0, 0)] double z,
-            [Values(1, 2, 0)] double t,
-            [Values(new [] {1, 0.41421356237282331, -2.414213562373142},
+
+
+        public class TestCase        {
+            [SetUp]
+            public void Setup()
+            {
+
+            }
+
+
+            [Test, Sequential]//Test case 1
+            public void TestSolvePTB3GivenRightArgumentsReturnsWell(
+                [Values(1, 0, 1)] double x,
+                [Values(1, 2, 0)] double y,
+                [Values(-3, 0, 0)] double z,
+                [Values(1, 2, 0)] double t,
+                [Values(new [] {1, 0.41421356237282331, -2.414213562373142},
                     new double[] {0.0, -0.0,-0.0},
                     new double[] {0}
-            )] double[] expected
-                     
-            )
-        {
-             {
-                Assert.That(expected, Is.EquivalentTo(PTB3.SolvePT3(x, y, z, t)));
-              }
-        }   
-    }
+            )] double[] expected) => Assert.That(expected, Is.EquivalentTo(PTB3.SolvePT3(x, y, z, t)));
 
+        }
     }
 }
+
 
